7ad0d2e7ad0d4b0145d75e6c0202a53be331a97f
Seungwoo Son <sson68x@gmail.com>
2022-05-01 21:51:19 -0700

    changed color theme and background photo

--- src/components/Header.js
+++ src/components/Header.js
@@ -59,7 +59,7 @@ class Header extends Component {
             <Switch 
               checked={this.state.checked}
               onChange={this.onThemeSwitchChange}
-              offColor="#baaa80"
+              offColor="#83c5be"
               onColor="#353535"
               className="react-switch mx-auto"
               width={90}
